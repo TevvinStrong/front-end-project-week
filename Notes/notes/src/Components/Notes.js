3351450dffe95a5197069350fa6d4252f66209a6
Tevvin <Tevvin7395@icloud.com>
2019-02-27 22:32:26 -0500

    Add some styling and components

---
+++ Notes/notes/src/Components/Notes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const notes = props => {
+    return (
+        <div className="notesContainer">
+
+        </div>
+    )
+}
+
+export default notes;
---
+++ Notes/notes/src/Components/SideView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SideView.css';
+
+const sideView = props => {
+    return (
+        <nav>
+            <h1>Lambda Notes</h1>
+            <button>View Your Notes</button>
+            <button>Create New Button</button>
+        </nav>
+    )
+}
+
+export default sideView;
